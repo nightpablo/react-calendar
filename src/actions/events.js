6dc41126c0bfdd29bcb7fba77c36af1fcbc9b9a3
nightpablo <pablo_barragan@hotmail.com>
2020-08-31 17:15:46 -0300

    Realizado calendario React

---
+++ src/actions/events.js
@@ -0,0 +1,108 @@
+import { types } from "../types/types";
+import { fetchConToken } from "../helpers/fetch";
+import { prepareEvents } from "../helpers/prepareEvents";
+import Swal from "sweetalert2";
+
+
+export const eventStartAddNew = ( event ) => {
+    return async ( dispatch, getState ) => {
+
+        const { uid, name } = getState().auth;
+        try {
+            const resp = await fetchConToken('events', event, 'POST');
+            const body = await resp.json();
+            
+            if ( body.ok ) {
+                event.id = body.evento.id;
+                event.user = {
+                    _id: uid,
+                    name: name
+                }
+                console.log(event);
+                dispatch( eventAddNew( event ) );
+            }
+        } catch (error) {
+            console.log(error);
+        }
+
+    };
+};
+
+const eventAddNew = ( event ) => ({
+    type: types.eventAddNew,
+    payload: event   
+});
+
+export const eventSetActive = ( event ) => ({
+    type: types.eventSetActive,
+    payload: event   
+});
+
+export const eventClearActiveEvent = () => ({
+    type: types.eventClearActiveEvent
+});
+
+export const eventStartUpdate = ( event ) => {
+    return async ( dispatch ) => {
+        try {
+            const resp = await fetchConToken(`events/${ event.id }`, event, 'PUT');
+            const body = await resp.json();
+
+            if ( body.ok ) {
+                dispatch( eventUpdated( event ) );
+            } else {
+                Swal.fire('Error', body.msg, 'error');
+            }
+            console.log(body);
+        } catch (error) {
+            console.log(error);
+        }
+    }
+}
+
+const eventUpdated = ( event ) => ({
+    type: types.eventUpdated,
+    payload: event
+});
+
+export const eventStartDelete = () => {
+    return async ( dispatch, getState ) => {
+        
+        const { activeEvent } = getState().calendar;
+        try {
+            const resp = await fetchConToken(`events/${ activeEvent.id }`, {}, 'DELETE');
+            const body = await resp.json();
+
+            if ( body.ok ) {
+                dispatch( eventDeleted() );
+            } else {
+                Swal.fire('Error', body.msg, 'error');
+            }
+            console.log(body);
+        } catch (error) {
+            console.log(error);
+        }
+    } 
+};
+
+const eventDeleted = () => ({ type: types.eventDeleted });
+
+export const eventStartLoading = () => {
+    return async ( dispatch ) => {
+
+        const resp = await fetchConToken('events');
+        const body = await resp.json();
+        
+        const events = prepareEvents(body.eventos);
+        // console.log(events);
+        dispatch( eventLoaded( events ) );
+
+    };
+}
+
+const eventLoaded = ( events ) => ({
+    type: types.eventLoaded,
+    payload: events
+});
+
+export const eventLogout = () => ({ type: types.eventLogout });
